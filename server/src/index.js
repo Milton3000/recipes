173a03227ffc68e30474968b5a96506814cbd217
MK3000 <kristofferssonmilton@gmail.com>
2024-03-21 20:01:57 +0100

    models folder + Users.js (Schemas)

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,11 @@ const app = express();
 
 
 // json middleware, will convert it to json. You wont get data from frontend without it.
+
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://MiltonK:test123@recipedatabase.i7qnalj.mongodb.net/")
+mongoose.connect("mongodb+srv://MiltonK:test123@recipedatabase.i7qnalj.mongodb.net/");
+
 
 app.listen(3001, () => console.log("SERVER STARTED!"));
