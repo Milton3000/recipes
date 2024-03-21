1da2cb5b775937a3a6543f978ac74fc72adc7117
MK3000 <kristofferssonmilton@gmail.com>
2024-03-21 20:29:15 +0100

    routes/user.js added router.post for /register and /login, testing if the API is working

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,10 +1,23 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
 
+router.post("/register", async (req, res) => {
+const { username, password } = req.body;
+
+// Making request to this collection here. It will return a promise so either try/catch or async/await.
+const user = await UserModel.findOne({ username });
+
+res.json(user);
+});
+
+
+
+router.post("/login");
 
 
 
