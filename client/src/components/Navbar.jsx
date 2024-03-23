1912d104f9c8417e88536daa1dd5103be77891c8
MK3000 <kristofferssonmilton@gmail.com>
2024-03-23 15:18:08 +0100

    useCookies hook @ Navbar, using if for checking login or button for logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+
 const Navbar = () => {
+
+const [cookies, setCookies] = useCookies(["access_token"]);
+
   return (
     <div className='navbar'>
 
         <Link to="/"> Home </Link>
         <Link to="/createrecipe"> Create Recipe </Link>
         <Link to="/savedrecipes"> Saved Recipes </Link>
-        <Link to="/auth"> Login/Register </Link>
+        {!cookies.access_token ? (<Link to="/auth"> Login/Register</Link>) : <button> Logout </button>}
     </div>
   )
 }
+
+// If there is no cookies.access_token it means that you are not logged in, so that means we show the <Link> for logging in, but if there is, it means you are already logged in and then we show a "Logout" Button.
 
 export default Navbar
