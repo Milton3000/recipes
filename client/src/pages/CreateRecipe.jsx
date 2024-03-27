7648bd6ea3bf32714e4d992bf15dfe9c47e78d19
MK3000 <kristofferssonmilton@gmail.com>
2024-03-27 11:03:39 +0100

    Added addIngredient function to handle the ingredients list and add more, also added handleIngredientChange function to handle it. Added these to the input field

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -12,15 +12,30 @@ const CreateRecipe = () => {
     userOwner: 0,
   });
 
+  //Quite a big form and all of the functions are the same so we create a handleChange that will do all of the logic.
   // Function below is just changing one specific part of the object.
-  const handleChange = (event) => {
+
+  const handleChange = (event, index) => {
+    const { value } = event.target;
+    const ingredients = recipe.ingredients; // Just making a copy of it.
+    ingredients[index] = value; // Change the element in the index equal to index.
+    setRecipe({ ...recipe, ingredients });
+  };
+
+    const handleIngredientChange = (event) => {
     const { name, value } = event.target;
     setRecipe({ ...recipe, [name]: value });
   };
 
-  //Quite a big form and all of the functions are the same so we create a handleChange that will do all of the logic.
 
   // Put a name property on each input (down below) so it serves as the key equal to the object information, so whenever we want to handle the change we can modify by accessing the name. Each of them refering to each specific object info.
+
+  // Setting the recipe to whatever it was before (...recipe). Called the Spread operator. 
+  // Whatever we put after the comma, is what's gonna change in the object. In this case we're changing the ingredients field. Initially it's an empty array, but now we're changing it to whatever the ingredients list was before (...spread operator again). So now it just adds an empty string to the end of the array. 
+  // We can generate the ingredients input based on the {recipe.ingredients list.} So we will .map the item. 
+  const addIngredient = () => {
+    setRecipe({...recipe, ingredients: [...recipe.ingredients, ""] });
+  }
 
   return (
     <div className='create-recipe'>
@@ -31,6 +46,16 @@ const CreateRecipe = () => {
         <input type='text' id='name' name='name' onChange={handleChange} />
 
         <label htmlFor='ingredients'> Ingredients </label>
+        {recipe.ingredients.map((ingredient, index) => (
+          <input 
+          key={index} 
+          type='text' 
+          name='ingredients' 
+          value={ingredient} 
+          onChange={(event) => handleIngredientChange(event, index)}/>
+        ))};
+
+        <button onClick={addIngredient}> Add Ingredient </button>
 
         <label htmlFor='instructions'> Inscructions </label>
         <textarea id='instructions' name='instructions' onChange={handleChange}></textarea>
