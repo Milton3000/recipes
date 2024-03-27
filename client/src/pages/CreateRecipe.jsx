782681bdc121c315f81b4284d1b74725ad284e34
MK3000 <kristofferssonmilton@gmail.com>
2024-03-27 10:50:47 +0100

    Added an object within useState for all the input information and handleChange function to target the previous recipe and then name property of the inputs.

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateRecipe = () => {
+
+  // An object containing all of the inputs
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: 0,
+  });
+
+  // Function below is just changing one specific part of the object.
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  }
+
+  //Quite a big form and all of the functions are the same so we create a handleChange that will do all of the logic.
+
+  // Put a name property on each input (down below) so it serves as the key equal to the object information, so whenever we want to handle the change we can modify by accessing the name. Each of them refering to each specific object info.
+
   return (
     <div className='create-recipe'>
 
       <h2> Create Recipe </h2>
       <form>
         <label htmlFor='name'> Name </label>
-        <input type='text' id='name' />
+        <input type='text' id='name' name='name' onChange={handleChange} />
 
         <label htmlFor='description'> Description </label>
         <textarea id='description' name='description'></textarea>
@@ -19,7 +40,7 @@ const CreateRecipe = () => {
 
         <label htmlFor='imageUrl'> Image URL </label>
         <input type='text' id='imageUrl' name='imageUrl' />
-        
+
         <label htmlFor='cookingTime'> Cooking Time (minutes) </label>
         <input type='number' id='cookingTime' name='cookingTime' />
       </form>
