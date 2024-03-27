e6047f02a4ef3b7ad58f50c818f8ba0f401f4447
MK3000 <kristofferssonmilton@gmail.com>
2024-03-27 10:40:58 +0100

    Back to Frontend for Recipes (CreateRecipe.jsx) added h2, labels and input for the categories

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const CreateRecipe = () => {
   return (
-    <div>Create Recipe</div>
+    <div className='create-recipe'>
+
+      <h2> Create Recipe </h2>
+      <form>
+        <label htmlFor='name'> Name </label>
+        <input type='text' id='name' />
+
+        <label htmlFor='description'> Description </label>
+        <textarea id='description' name='description'></textarea>
+
+        <label htmlFor='ingredients'> Ingredients </label>
+
+        <label htmlFor='instructions'> Inscructions </label>
+        <textarea id='instructions' name='instructions'></textarea>
+
+        <label htmlFor='imageUrl'> Image URL </label>
+        <input type='text' id='imageUrl' name='imageUrl' />
+        
+        <label htmlFor='cookingTime'> Cooking Time (minutes) </label>
+        <input type='number' id='cookingTime' name='cookingTime' />
+      </form>
+    </div>
   )
 }
 
