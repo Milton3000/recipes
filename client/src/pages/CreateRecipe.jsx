76895b9743a6cc87b320fd0e7a5e0749dc0dd60f
MK3000 <kristofferssonmilton@gmail.com>
2024-03-28 09:48:06 +0100

    fixed function error, typed wrong

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const CreateRecipe = () => {
 
@@ -15,16 +16,17 @@ const CreateRecipe = () => {
   //Quite a big form and all of the functions are the same so we create a handleChange that will do all of the logic.
   // Function below is just changing one specific part of the object.
 
-  const handleChange = (event, index) => {
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleIngredientChange = (event, index) => {
     const { value } = event.target;
     const ingredients = recipe.ingredients; // Just making a copy of it.
     ingredients[index] = value; // Change the element in the index equal to index.
     setRecipe({ ...recipe, ingredients });
-  };
-
-    const handleIngredientChange = (event) => {
-    const { name, value } = event.target;
-    setRecipe({ ...recipe, [name]: value });
   };
 
 
@@ -34,28 +36,37 @@ const CreateRecipe = () => {
   // Whatever we put after the comma, is what's gonna change in the object. In this case we're changing the ingredients field. Initially it's an empty array, but now we're changing it to whatever the ingredients list was before (...spread operator again). So now it just adds an empty string to the end of the array. 
   // We can generate the ingredients input based on the {recipe.ingredients list.} So we will .map the item. 
   const addIngredient = () => {
-    setRecipe({...recipe, ingredients: [...recipe.ingredients, ""] });
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
   }
 
-  console.log(recipe)
-
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/recipes", recipe);
+      alert("Recipe Created");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(recipe.ingredients);
   return (
     <div className='create-recipe'>
 
       <h2> Create Recipe </h2>
-      <form>
+      <form onSubmit={onSubmit}>
         <label htmlFor='name'> Name </label>
         <input type='text' id='name' name='name' onChange={handleChange} />
 
-        <label htmlFor='ingredients'> Ingredients </label>
+        <label htmlFor="ingredients">Ingredients</label>
         {recipe.ingredients.map((ingredient, index) => (
-          <input 
-          key={index} 
-          type='text' 
-          name='ingredients' 
-          value={ingredient} 
-          onChange={(event) => handleIngredientChange(event, index)}/>
-        ))};
+          <input
+            key={index}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(event) => handleIngredientChange(event, index)}
+          />
+        ))}
 
         <button onClick={addIngredient} type='button'> Add Ingredient </button>
 
@@ -67,7 +78,7 @@ const CreateRecipe = () => {
 
         <label htmlFor='cookingTime'> Cooking Time (minutes) </label>
         <input type='number' id='cookingTime' name='cookingTime' onChange={handleChange} />
-        
+
         <button type='submit'> Create Recipe </button>
       </form>
     </div>
