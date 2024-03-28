12dd1e9f54b4e6590150304108985df8299e7edb
MK3000 <kristofferssonmilton@gmail.com>
2024-03-28 11:12:18 +0100

    Started Home.jsx, added useState + useEffect to get the recipes and then map through them to show them

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchRecipe();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div> 
+
+      <h1> Recipes </h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2> {recipe.name} </h2>
+            </div>
+            <div className='instructions'>
+          <p> {recipe.instructions} </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
-}
+};
 
 export default Home
