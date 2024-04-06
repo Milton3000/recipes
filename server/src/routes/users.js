e88e0e19c809a2aecf0718b400795da3768f803b
MK3000 <kristofferssonmilton@gmail.com>
2024-04-06 11:00:32 +0200

    verifyToken middlewere implemented for authentication

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -43,3 +43,19 @@ router.post("/login", async (req, res) => {
 });
 
 export { router as userRouter };
+
+
+// Middlewere for authentication
+export const verifyToken = (req, res, next) => {
+const token = req.headers.authorization;
+if (token) {
+  jwt.verify(token, "secret", (err) => {
+    if (err) return res.sendStatus(403);
+    next();
+  });
+} else {
+  res.sendStatus(401);
+}
+};
+
+// 403 not authorized user
